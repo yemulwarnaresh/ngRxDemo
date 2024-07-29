6906f82021536b00317135e7b7cf119268ad20f9
yemulwarnaresh <yemulwarnaresh@gmail.com>
2024-07-29 11:24:20 +0530

    changes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; //  Testing
 import { RouterModule, Routes } from '@angular/router';
+import { addComponent } from './components/add/add.component';
+import { layoutComponent } from './components/layout/layout.component'; 
+import { updateComponent } from './components/update/update.component'; 
+import { OtherComponent } from './components/other/other.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'home-layout', pathMatch: 'full' },
+  { path: '', component: layoutComponent },
+  { path: 'add', component: addComponent },
+  { path: 'update/:id', component: updateComponent },
+  { path : 'other', component:OtherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
