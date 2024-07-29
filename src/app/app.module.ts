8d68d605c2b8be979bddda90b66feae3e0b952a1
yemulwarnaresh <yemulwarnaresh@gmail.com>
2024-07-29 10:19:04 +0530

    app.module.ts changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { addComponent } from './components/add/add.component';
+import { updateComponent } from './components/update/update.component';
+import { layoutComponent } from './components/layout/layout.component';
+
+import { reducers } from './ngrx/store';
+import { StoreModule } from '@ngrx/store';
+import { OtherComponent } from './components/other/other.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    addComponent,
+    updateComponent,
+    layoutComponent,
+    OtherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers)
   ],
   providers: [],
   bootstrap: [AppComponent]
